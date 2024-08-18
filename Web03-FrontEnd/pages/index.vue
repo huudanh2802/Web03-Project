5b87670100e5e29ec0fe6aaacb82144f972c937b
Danh Hữu <56262591+huudanh2802@users.noreply.github.com>
2024-08-18 14:08:52 +0700

    Clean

---
+++ Web03-FrontEnd/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const mes = ref(0);
+function alertMes() {
+  mes.value++;
+  alert(mes.value);
+}
+</script>
+<template>
+  <div class="container flex justify-center">
+    <button type="button" @click="alertMes">Test</button>
+  </div>
+</template>
