f1ffbe3b11186fd510b027bd7f7a4b9783fcf917
Danh Hữu <56262591+huudanh2802@users.noreply.github.com>
2024-11-18 11:49:44 +0700

    Init frontend

---
+++ web03-frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+
+    <div>
+      <Button className="bg-blue-700">Click me</Button>
+    </div>
+  );
+}
