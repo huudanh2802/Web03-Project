a4bfb3c6147d29e1391b9d8540fcd4797ed20556
Danh Hữu <56262591+huudanh2802@users.noreply.github.com>
2024-12-12 16:26:17 +0700

    Update signin-dialog.tsx

--- web03-frontend/src/containers/signin/signin-dialog.tsx
+++ web03-frontend/src/containers/signin/signin-dialog.tsx
@@ -29,24 +29,16 @@ export default function SigninDialog({
         </DialogHeader>
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
-              id="name"
-              defaultValue="Pedro Duarte"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="username" className="text-right">
               Username
             </Label>
-            <Input
-              id="username"
-              defaultValue="@peduarte"
-              className="col-span-3"
-            />
+            <Input id="username" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="password" className="text-right">
+              Password
+            </Label>
+            <Input id="password" type="password" className="col-span-3" />
           </div>
         </div>
         <DialogFooter className="flex row-auto justify-center">
