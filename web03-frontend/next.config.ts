f612c5fece474c870323a6a1777e187a3756c9c2
Danh Hữu <56262591+huudanh2802@users.noreply.github.com>
2025-01-02 14:33:01 +0700

    FE docker-compose

--- web03-frontend/next.config.ts
+++ web03-frontend/next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
+module.exports = {
+  // ... rest of the configuration.
+  output: "standalone",
+};
 const nextConfig: NextConfig = {
   /* config options here */
 };
