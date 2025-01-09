3d69f9176c927b67566b5f171fade55072a77c05
Danh Hữu <56262591+huudanh2802@users.noreply.github.com>
2025-01-09 14:18:47 +0700

    add env

--- web03-backend/src/main/java/com/web03backend/config/WebConfig.java
+++ web03-backend/src/main/java/com/web03backend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Add your allowed origins here
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
