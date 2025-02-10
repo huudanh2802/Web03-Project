644e778af4f1cc595ab17097e60f99ab0ed4da38
Danh Hữu <56262591+huudanh2802@users.noreply.github.com>
2025-02-10 17:43:46 +0700

    Refactor BE docker

--- web03-backend/src/main/java/com/web03backend/repositories/spec/ITestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.web03backend.repositories.spec;
-
-import com.web03backend.domain.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ITestRepository extends JpaRepository<TestEntity, Long> {
-}
